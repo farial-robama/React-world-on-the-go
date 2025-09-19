f92480093fafd2cf973ed58c8759ff275f3a7941
farial-robama <farialrobama15@gmail.com>
2025-09-19 19:11:32 +0600

    Added visited and not visited countries functionalities and styles

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css';
 
 const Country = ({ country }) => {
+    const [visited, setVisited] = useState(false);
+
     // console.log(country.population.population);
+    const handleVisited = () =>{
+        // basic system
+    // if (visited) {
+    //     setVisited(false)
+    // }else{
+    //     setVisited(true)
+    // }
+    // second system
+    // setVisited(visited ? false : true);
+    // third system
+    setVisited(!visited)   
+}
     
     return (
-        <div>
+        <div className= {`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.png} />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
-            
+            <p>Area: {country.area.area} 
+                {country.area.area > 300000 ? "Big Country" : "Small Country"} </p>
+            <button onClick={handleVisited} >
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
