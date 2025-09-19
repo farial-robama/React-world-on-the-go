f7fe066378c8af415262a8c1febed7e08ef841ea
farial-robama <farialrobama15@gmail.com>
2025-09-19 16:33:06 +0600

    display country details with flags and population

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Country = ({ country }) => {
+    // console.log(country.population.population);
+    
+    return (
+        <div>
+            <img src={country.flags.flags.png} alt={country.flags.flags.png} />
+            <h3>Name: {country.name.common}</h3>
+            <p>Population: {country.population.population}</p>
+            
+        </div>
+    );
+};
+
+export default Country;
