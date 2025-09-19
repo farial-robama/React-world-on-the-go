7f80d39e24da22d702ff99904e966c745c987021
farial-robama <farialrobama15@gmail.com>
2025-09-19 23:16:48 +0600

    add visited flags feature with state and display

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Country.css';
 
-const Country = ({ country, handleVisitedCountries }) => {
+const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
     const [visited, setVisited] = useState(false);
 
     // console.log(country.population.population);
@@ -29,6 +29,7 @@ const Country = ({ country, handleVisitedCountries }) => {
             <button onClick={handleVisited} >
                 {visited ? 'Visited' : 'Not Visited'}
             </button>
+            <button onClick={() =>{ handleVisitedFlag(country?.flags?.flags?.png) }}>Add Visited Flag</button>
         </div>
     );
 };
